8a14b8d35a7ed3201889fb8e3c1808d6e2b38f14
NES3D <160732206+NES3D@users.noreply.github.com>
2024-04-12 15:17:18 +0400

    Add files via upload

---
+++ EgorNesmeyanov_myHomework_week_01.py
@@ -0,0 +1,12 @@
+import maya.cmds as cmds
+sph = cmds.polySphere(r=1, name="MySphere")[0]
+minTime = cmds.playbackOptions(query=1, minTime=True)
+maxTime = cmds.playbackOptions(query=1, maxTime=True)
+cmds.setKeyframe(sph + ".translateX", time=minTime, value=-10)
+cmds.setKeyframe(sph + ".translateX", time=maxTime, value=10)
+
+cube = cmds.polyCube(name="MyCube")[0]
+cmds.move(-10,0,10)
+cmds.parentConstraint(sph, cube, maintainOffset=True)
+cmds.bakeResults(cube + ".translateX", simulation=True, time=(minTime,maxTime))
+cmds.delete(cube, constraints=True)
