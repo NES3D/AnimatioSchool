7f479e70ca7afa3528ffb5aaa16534c78f7efce4
NES3D <160732206+NES3D@users.noreply.github.com>
2024-04-12 15:33:57 +0400

    EgorNesmeyanov_myHomework_week_01.py

--- EgorNesmeyanov_myHomework_week_01.py
+++ EgorNesmeyanov_myHomework_week_01.py
@@ -2,6 +2,7 @@ import maya.cmds as cmds
 sph = cmds.polySphere(r=1, name="MySphere")[0]
 minTime = cmds.playbackOptions(query=1, minTime=True)
 maxTime = cmds.playbackOptions(query=1, maxTime=True)
+cmds.currentTime(minTime, edit=True)
 cmds.setKeyframe(sph + ".translateX", time=minTime, value=-10)
 cmds.setKeyframe(sph + ".translateX", time=maxTime, value=10)
 
