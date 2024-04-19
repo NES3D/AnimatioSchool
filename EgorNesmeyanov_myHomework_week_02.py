0b56d18623ad42cc06280a1dec7ab019d842aa25
NES3D <egor_nesmeyanov@mail.ru>
2024-04-19 23:04:46 +0300

    Update EgorNesmeyanov_myHomework_week_02.py

--- EgorNesmeyanov_myHomework_week_02.py
+++ EgorNesmeyanov_myHomework_week_02.py
@@ -1,6 +1,7 @@
 import maya.cmds as cmds
 import random
 
+# Очистка сцены от старой геометрии
 def clearScene():
     allSpheres = cmds.ls(type = "mesh")
     allSpheres = cmds.listRelatives(allSpheres, parent=1)
@@ -8,61 +9,79 @@ def clearScene():
 
 clearScene()
 
+# Функция создания планеты
 def createPlanet(planetRadiusMin, planetRadiusMax, planetName, moonsMin, moonsMax, moonRadiusMin, moonRadiusMax):
 
+    # Установка минимальных и максимальных значений для параметров функции
     planetRadius = random.randint (planetRadiusMin, planetRadiusMax)
 
     moons = random.randint (moonsMin, moonsMax)
 
     moonRadius = random.uniform (moonRadiusMin, moonRadiusMax)
 
+    # Создание главной планеты
     cmds.polySphere(name = planetName, r = planetRadius)
     
+    # Определение первого и последнего кадра на таймлайне
     minTime = cmds.playbackOptions(query=1, minTime=True)
     
     maxTime = cmds.playbackOptions(query=1, maxTime=True)
     
+    # Установка текущего значения времени в минильное положение
     cmds.currentTime(minTime, edit=True)
     
+    # Анимация вращения главной планеты
     cmds.setKeyframe(planetName + ".rotateY", time=minTime, value=0)
     
     cmds.setKeyframe(planetName + ".rotateY", time=maxTime, value=360)
 
+    # Установка типа анимации Linear
     cmds.keyTangent(inTangentType = "linear", outTangentType = "linear")
- 
+    
+    # Цикл по созданию лун
     for i in range (moons):
 
+        # Установка минимальных и максимальных значений радиусов лун
         moonRadius = random.uniform (moonRadiusMin, moonRadiusMax)
         
+        # Создание Луны
         s = cmds.polySphere(r = moonRadius)[0]
 
         r = cmds.polySphere(s, query=1, r=1)
 
+        # Установка дистанции для первой луны
         if i == 0:
 
             distance = planetRadius + moonRadius * 1.5
-                   
+
+        # Установка дистанции для всех последующих лун           
         else:
 
             distance = distance + r
-               
+       
         cmds.xform(s, translation = [distance, 0, 0])
+
+        # Создание оффсет групп для лун
+        moonGRP = cmds.group(s)
         
-        cmds.xform(ws = 1, a = 1, rp = [0, 0, 0])
+        # Установка пивота для оффсетгрупп в мировое начало координат
+        cmds.xform(moonGRP, ws = 1, a = 1, rp = [0, 0, 0])
         
-        rotateZ = random.randint (-45, 20)
+        # Расстановка лун вокруг главной Планеты
+        rotateZ = random.randint (-45, 45)
         
         rotateY = random.randint (0, 360)
      
-        cmds.xform(rotation = [0, rotateY, rotateZ])
-                
-        cmds.parent(s, planetName)
+        cmds.xform(moonGRP, rotation = [0, rotateY, rotateZ])
 
-        # Animate Moons
+        # Парент лун и их оффсет групп к главной Планете        
+        cmds.parent(moonGRP, planetName)
 
-        cmds.setKeyframe(s + ".rotateY", time=minTime, value= rotateY)
+        # Анимация лун
+
+        cmds.setKeyframe(moonGRP + ".rotateY", time=minTime, value= rotateY)
     
-        cmds.setKeyframe(s + ".rotateY", time=maxTime, value= rotateY + 3240)
+        cmds.setKeyframe(moonGRP + ".rotateY", time=maxTime, value= rotateY + 3240)
 
         cmds.keyTangent(inTangentType = "linear", outTangentType = "linear")
 
